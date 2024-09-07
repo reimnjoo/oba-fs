b50cd7cfe97d802eb374ac00b45b8d12ced470f9
= <=>
2024-09-08 01:35:23 +0800

    feat: added the separated form into its own login form from login.tsx

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,39 @@
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { LoginType } from "../types/_types";
+
+export default function LoginForm() {
+
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<LoginType>();
+
+    const onSubmit: SubmitHandler<LoginType> = (data) => {
+        login(data);
+        navigate("/dashboard");
+        console.log(data);
+    };
+
+    return (
+        <form id="login-form" className="" onSubmit={handleSubmit(onSubmit)}>
+            <input
+                type="text"
+                placeholder="Your email address"
+                {...register("email")}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                {...register("password")}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
