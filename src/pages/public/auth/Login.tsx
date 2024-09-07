77c9675129d7da4adf8f4ee4e36b1c39f41b2e6f
= <=>
2024-09-08 01:33:51 +0800

    feat: separated the login form in a new component for maintainability

--- src/pages/public/auth/Login.tsx
+++ src/pages/public/auth/Login.tsx
@@ -1,20 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../../context/AuthContext"; 
+import LoginForm from "../../../components/LoginForm";
 
 export default function Login() {
-
-    const { login } = useAuth(); 
-    const navigate = useNavigate();
-
-    const handleLogin = () => {
-        login();
-        navigate('/dashboard'); 
-    };
-
     return (
         <div className="font-poppins">
             <p>Login</p>
-            <button onClick={handleLogin}>Login Here</button>
+            <LoginForm />
         </div>
     );
 }
