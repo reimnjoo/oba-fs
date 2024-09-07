6e442aae7d125e1f73a1e8b091cd1cf45d580d83
= <=>
2024-09-08 01:32:20 +0800

    feat: added temporary login function to test react hook form

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState, ReactNode } from "react";
 
 export interface AuthContextType {
     isAuthenticated: boolean;
-    login: () => void;
+    login: (data: { email: string; password: string }) => void;
     logout: () => void;
 }
 
@@ -13,7 +13,19 @@ export const AuthContext = createContext<AuthContextType | undefined>(
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const login = () => setIsAuthenticated(true);
+    // const login = () => setIsAuthenticated(true);
+    // TEMPORARY LOGIN TO TEST REACT HOOK FORM
+    const login = (data: { email: string, password: string }) => {
+
+        if(!data.email && !data.password) console.log("Login credentials required!");
+        if(data.email === "aladiahfulminar1@gmail.com" && data.password === "123") {
+            setIsAuthenticated(true);
+            console.log("Successfully logged in.")
+        } else {
+            console.log("Invalid credentials.");
+        }
+    }
+
     const logout = () => setIsAuthenticated(false);
 
     return (
